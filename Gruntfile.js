323c6b538e6ef27bfeeaa3304b6fe921da22edcc
Mehuge <adf@sorcerer.co.uk>
2017-11-15 19:38:31 +0000

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    uncss: {
+      dist: {
+        options: {
+          ignoreSheets : [/fonts.googleapis/],
+        },
+        files: [
+        { src: 'index.html', dest: 'cleancss/tidy.css' }
+        ]
+      }
+    },
+    cssmin: {
+      dist: {
+        files: [
+        { src: 'cleancss/tidy.css', dest: 'cleancss/tidy.css' }
+        ]
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-uncss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default tasks.
+  grunt.registerTask('default', ['uncss', 'cssmin']);
+
+};
